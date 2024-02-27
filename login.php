36e9b8e0ddd20798b04da5bf1ee96301390bb0b6
Ermmyna Roselee <ermmyna@gmail.com>
2024-02-27 15:27:43 +0800

    Update login.php

--- login.php
+++ login.php
@@ -34,136 +34,43 @@ if (isset($_SESSION['success_message'])) {
     <div class="wrapper">
         <!--Header Start-->
         <header class="header-style-2">
-           <nav class="navbar navbar-expand-lg">
-              <a class="navbar-brand" href="index-2.html"><img src="images/h2logo.png" alt=""></a>
-              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <i class="fas fa-bars"></i> </button>
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                 <ul class="navbar-nav mr-auto">
-                    <li class="nav-item dropdown">
-                       <a class="nav-link dropdown-toggle" href="index-2.html"  role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Home </a>
-                       <ul class="dropdown-menu" >
-                          <li><a href="index-2.html">Home One</a></li>
-                          <li><a href="home-two.html">Home Two</a></li>
-                          <li><a href="home-three.html">Home Three</a></li>
-                       </ul>
-                    </li>
-                    <li class="nav-item"> <a class="nav-link" href="about.html">About</a> </li>
-                    <li class="nav-item dropdown">
-                       <a class="nav-link dropdown-toggle" href="events-grid.html"  role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Events </a>
-                       <ul class="dropdown-menu" >
-                          <li><a href="events-grid.html">Events Grid</a></li>
-                          <li><a href="events-grid-2.html">Events Grid Two</a></li>
-                          <li><a href="events-grid-3.html">Events Grid Three</a></li>
-                          <li><a href="events-list.html">Events List</a></li>
-                          <li><a href="events-list-two.html">Events List Two</a></li>
-                          <li><a href="event-details.html">Event Details</a></li>
-                       </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                       <a class="nav-link dropdown-toggle" href="causes.html"  role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> EduHub </a>
-                       <ul class="dropdown-menu" >
-                          <li><a href="causes.html">Causes Grid</a></li>
-                          <li><a href="causes-list.html">Causes List</a></li>
-                          <li><a href="causes-details.html">Causes Details</a> </li>
-                       </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                       <a class="nav-link dropdown-toggle" href="blog.html"  role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Social </a>
-                       <ul class="dropdown-menu" >
-                          <li><a href="blog.html">Blog Default</a></li>
-                          <li><a href="blog-list.html">Blog List</a> </li>
-                          <li><a href="blog-grid.html">Blog Grid</a></li>
-                          <li><a href="blog-two-col.html">Blog Two Columns</a> </li>
-                          <li><a href="blog-three-col.html">Blog Three Columns</a></li>
-                          <li><a href="blog-details.html">Blog Details</a></li>
-                       </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                       <a class="nav-link dropdown-toggle" href="#"  role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Pages </a>
-                       <ul class="dropdown-menu" >
-                          <li>
-                             <a href="#">Projects</a>
-                             <ul class="dropdown-menu" >
-                                <li><a href="projects.html">Projects</a> </li>
-                                <li><a href="projects-grid.html">Projects Grid</a> </li>
-                                <li><a href="projects-grid-two.html">Projects Grid Two</a> </li>
-                                <li><a href="projects-list.html">Projects List</a> </li>
-                                <li><a href="projects-details.html">Project Details</a> </li>
-                             </ul>
-                          </li>
-                          <li>
-                             <a href="#">Shop</a>
-                             <ul class="dropdown-menu" >
-                                <li><a href="shop.html">Shop</a> </li>
-                                <li><a href="shop-two.html">Shop Two</a> </li>
-                                <li><a href="shop-details.html">Shop Details</a> </li>
-                             </ul>
-                          </li>
-                          <li>
-                             <a href="team.html">Team</a>
-                             <ul class="dropdown-menu" >
-                                <li><a href="team.html">Team One</a> </li>
-                                <li><a href="team-two.html">Team Two</a> </li>
-                                <li><a href="team-details.html">Team Details</a> </li>
-                             </ul>
-                          </li>
-                          <li>
-                             <a href="#">Gallery</a>
-                             <ul class="dropdown-menu" >
-                                <li><a href="gallery-grid.html">Gallery Grid</a> </li>
-                                <li><a href="gallery-full.html">Gallery Full</a> </li>
-                                <li><a href="gallery-masonry.html">Gallery Masonry</a> </li>
-                             </ul>
-                          </li>
-                          <li><a href="testimonials.html">Testimonials</a> </li>
-                          <li><a href="donation.html">Donation</a> </li>
-                          <li><a href="my-account.html">My Account</a> </li>
-                          <li><a href="coming-soon.html">Coming Soon</a> </li>
-                          <li><a href="page-404.html">404 Error</a> </li>
-                       </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                       <a class="nav-link dropdown-toggle" href="contact.html"  role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Contact </a>
-                       <ul class="dropdown-menu" >
-                          <li><a href="contact-one.html">Contact One</a> </li>
-                          <li><a href="contact-two.html">Contact Two</a> </li>
-                       </ul>
-                    </li>
-                 </ul>
-                 <ul class="topnav-right">
-                    <li> <a class="mdonate" href="donation.html"><span>Make a Donation</span></a> </li>
-                    <li> <a class="search-icon" href="#search"> <i class="fas fa-search"></i> </a> </li>
-                    <li class="dropdown">
-                       <a class="cart-icon" href="#" role="button" id="cartdropdown" data-toggle="dropdown"> <i class="fas fa-shopping-cart"></i></a>
-                       <div class="dropdown-menu cart-box" aria-labelledby="cartdropdown">
-                          Recently added item(s)
-                          <ul class="list">
-                             <li class="item">
-                                <a href="#" class="preview-image"><img class="preview" src="images/pro.jpg" alt=""></a>
-                                <div class="description"> <a href="#">Sample Course</a> <strong class="price">1 x $44.95</strong> </div>
-                             </li>
-                             <li class="item">
-                                <a href="#" class="preview-image"><img class="preview" src="images/pro.jpg" alt=""></a>
-                                <div class="description"> <a href="#">Sample Course</a> <strong class="price">1 x $44.95</strong> </div>
-                             </li>
-                          </ul>
-                          <div class="total">Total: <strong>$44.95</strong></div>
-                          <div class="view-link"><a href="#">Proceed to Checkout</a> <a href="#">View cart </a></div>
-                       </div>
-                    </li>
-                    <li class="login-reg"> <a href="my-account.html">Login</a> | <a href="my-account.html">Signup</a> </li>
-                 </ul>
-              </div>
-           </nav>
-        </header>
-        <div id="search">
-           <button type="button" class="close">×</button>
-           <form class="search-overlay-form">
-              <input type="search" value="" placeholder="type keyword(s) here" />
-              <button type="submit" class="btn btn-primary"><i class="fas fa-search"></i></button>
-           </form>
-        </div>
-        <!--Header End-->
+            <nav class="navbar navbar-expand-lg">
+               <a class="logo" href="index.html"><img src="images/EcoTrace Logo.png" alt="" style="height: 100px"></a>
+               <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <i class="fas fa-bars"></i> </button>
+               <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                   <ul class="navbar-nav mr-auto">
+                       <li class="nav-item">
+                           <a class="nav-link" href="index-2.html">Home</a>
+                       </li>
+                       <li class="nav-item">
+                           <a class="nav-link" href="about.html">About</a>
+                       </li>
+                       <li class="nav-item">
+                           <a class="nav-link" href="events-grid.html">Events</a>
+                       </li>
+                       <li class="nav-item">
+                           <a class="nav-link" href="causes.html">Causes</a>
+                       </li>
+                       <li class="nav-item">
+                           <a class="nav-link" href="blog.html">Blogs</a>
+                       </li>
+                       <li class="nav-item">
+                           <a class="nav-link" href="#">Pages</a>
+                       </li>
+                       <li class="nav-item">
+                           <a class="nav-link" href="contact.html">Contact</a>
+                       </li>
+                   </ul>
+
+                   <li class="nav-item" style="list-style: none;">
+                     <a class="login-btn" href="login-page" role="button"> Login </a>
+                 </li>
+               </div>
+         
+            </nav>
+            
+         </header>
+         <!-- Header End -->
         <!--Inner Header Start-->
         <section class="wf100 p100 inner-header">
            <div class="container">
@@ -175,7 +82,7 @@ if (isset($_SESSION['success_message'])) {
            </div>
         </section>
         <!--Inner Header End--> 
-        <!--Causes Start-->
+        <!--Content Start-->
         <section class="wf100 p80">
            <div class="container">
               <div class="row">
@@ -251,7 +158,7 @@ if (isset($_SESSION['success_message'])) {
               </div>
            </div>
         </section>
-        <!--Causes End--> 
+        <!-- Content End--> 
 
          <!-- Popup message -->
     <div id="popupMessage" class="modal" style="display: none;">
@@ -276,10 +183,75 @@ if (isset($_SESSION['success_message'])) {
         </div>
     </div>
 
-    </div>
-    <?php if (isset($success_message)): ?>
-   <div class="alert alert-success"><?php echo $success_message; ?></div>
-   <?php endif; ?>
+   </div>
+   <div>
+      
+   </div>
+   <footer class="footer-03">
+      <div class="container">
+            <div class="row">
+               <div class="col-md-6">
+                  <div class="d-flex align-items-center justify-content-between mb-4">
+                        
+                     <div class="logo-space">
+                           <img src="images/EcoTrace Logo.png" alt="Eco Trace Logo" class="logo-img">
+                     </div>
+                  </div>
+      
+                  <div class="row">
+                        <div class="col-md-6 mb-md-0 mb-4">
+                           <h2 class="footer-heading">Carbon Calculator</h2>
+                           <ul class="list-unstyled">
+                              <li><a href="#" class="py-1 d-block">How it Works</a></li>
+                              <li><a href="#" class="py-1 d-block">Log Your Activities</a></li>
+                              <li><a href="#" class="py-1 d-block">Reduce Your Footprint</a></li>
+                           </ul>
+                        </div>
+                        <div class="col-md-4 mb-md-0 mb-4">
+                           <h2 class="footer-heading">Resources</h2>
+                           <ul class="list-unstyled">
+                              <li><a href="#" class="py-1 d-block">Blog</a></li>
+                              <li><a href="#" class="py-1 d-block">Educational Materials</a></li>
+                              <li><a href="#" class="py-1 d-block">FAQs</a></li>
+                           </ul>
+                        </div>
+                  </div>
+               </div>
+               <div class="col-md-6">
+                  <div class="row justify-content-end">
+                        <div class="col-md-12 col-lg-11 mb-md-0 mb-4">
+                           
+                           <h2 class="footer-heading mt-5">Connect With Us</h2>
+                           <ul class="ftco-footer-social p-0">
+                              <li class="ftco-animate"><a href="#" data-toggle="tooltip" data-placement="top" title="Twitter"><i class="fab fa-twitter"></i></a></li>
+                              <li class="ftco-animate"><a href="#" data-toggle="tooltip" data-placement="top" title="Facebook"><i class="fab fa-facebook"></i></a></li>
+                              <li class="ftco-animate"><a href="#" data-toggle="tooltip" data-placement="top" title="Instagram"><i class="fab fa-instagram"></i></a></li>
+                              <li class="ftco-animate"><a href="#" data-toggle="tooltip" data-placement="top" title="LinkedIn"><i class="fab fa-linkedin"></i></a></li>
+                           </ul>
+                           <h2 class="footer-heading mt-5">Subscribe to Our Newsletter</h2>
+                           <form action="#" class="subscribe-form">
+                              <div class="form-group d-flex">
+                                    <input type="text" class="form-control rounded-left" placeholder="Enter your email address">
+                                    <input type="submit" value="Subscribe" class="form-control submit px-3 rounded-right">
+                              </div>
+                           </form>
+                        </div>
+                  </div>
+               </div>
+            </div>
+            <div class="row mt-5 pt-4 border-top">
+               <div class="col-md-6 col-lg-8">
+                  <p class="copyright">© <script>document.write(new Date().getFullYear());</script> All rights reserved | EcoTrace - Track and Reduce Your Carbon Footprint</p>
+               </div>
+               <div class="col-md-6 col-lg-4 text-md-right">
+                  <p class="mb-0 list-unstyled">
+                        <a class="mr-md-3" href="#">Terms &amp; Conditions</a>
+                        <a class="mr-md-3" href="#">Privacy Policy</a>
+                  </p>
+               </div>
+            </div>
+      </div>
+   </footer>
 
     <!--   JS Files Start  --> 
     <script src="js/jquery-3.3.1.min.js"></script> 
